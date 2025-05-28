20249fc72a18db9b8965578dd8c32e7c679f0c01
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2025-05-28 12:22:54 +0600

    add move zeroes solution in python

--- solutions/arrays/move_zeroes.py
+++ solutions/arrays/move_zeroes.py
@@ -1,4 +1,13 @@
-a = 10
-b = 20
-c = a + b
-print(c)
+# Problem: Move Zeroes
+# Link: https://leetcode.com/problems/move-zeroes/
+# Time: O(n)
+# Space: O(1)
+
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        pos = 0
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[i], nums[pos] = nums[pos], nums[i]
+                pos += 1
