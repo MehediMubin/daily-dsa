dae008cb1dbaa642591bb53adf0399892f5d3a44
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2025-05-28 16:24:42 +0600

    add majority element solution (Boyer Moore's Voting Algorithm)

--- solutions/arrays/majority_element.py
+++ solutions/arrays/majority_element.py
@@ -8,3 +8,21 @@ class Solution:
     def majorityElement(self, nums: List[int]) -> int:
         nums.sort()
         return nums[len(nums) // 2]
+
+
+# Boyer Moore's Voting Algorithm
+# Time Complexity: O(n)
+# Space Complexity: O(1)
+
+class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        cnt = 0
+        candidate = None
+
+        for num in nums:
+            if cnt == 0:
+                candidate = num
+            
+            cnt += (1 if num == candidate else -1)
+        
+        return candidate
