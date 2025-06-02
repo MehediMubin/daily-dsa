94c062d2683391c0f39fbded8233ea15ae3d1fef
Mehedi Hasan Mubin <mehedi.cseulab201@gmail.com>
2025-06-02 19:10:51 +0600

    update solution file

--- solutions/arrays/best_time_to_b&s.go
+++ solutions/arrays/best_time_to_b&s.go
@@ -1,3 +1,8 @@
+// Problem: Best Time to Buy and Sell Stock
+// Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+// Time Complexity: O(n)
+// Space Complexity: O(1) 
+
 func max(a, b int) int {
     if a > b {
         return a
